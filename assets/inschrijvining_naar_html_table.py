5ab5145148b4357da52ef94e744cbcecd5a53e79
wvflevo <wvflevo.wedstrijdcommissie@gmail.com>
2025-09-01 20:00:49 +0200

    ruff format

--- assets/inschrijvining_naar_html_table.py
+++ assets/inschrijvining_naar_html_table.py
@@ -4,12 +4,14 @@ import sys
 
 PATH = pathlib.Path(__file__).parent
 
+
 def main():
     csv_file = pathlib.Path(
-        PATH / "./Wolderwijdcup (Antwoorden) - Formulierreacties 1 2024-09-03.csv"
+        PATH
+        / "./Wolderwijdcup (Antwoorden) - Formulierreacties 1 2024-09-03.csv"
     )
     if len(sys.argv) == 1:
-        print('Give input file')
+        print("Give input file")
         return None
     file = sys.argv[1]
     csv_file = pathlib.Path(PATH / file)
@@ -21,18 +23,22 @@ def main():
     html_table = df[["Wedstrijdnummer", "Bootnaam", "Scheepstype"]]
 
     # Convert the DataFrame to HTML with the desired table structure
-    html_table = html_table.to_html(classes="table table-hover", index=False, border=0)
+    html_table = html_table.to_html(
+        classes="table table-hover", index=False, border=0
+    )
 
     # Add custom classes to the table structure
-    html_table = html_table.replace('<thead>', '<thead class="table-secondary">')
+    html_table = html_table.replace(
+        "<thead>", '<thead class="table-secondary">'
+    )
 
     # Wrap the table in a div with class "table-responsive"
     html_table = f'<div class="table-responsive">{html_table}</div>'
-    html_table = html_table.replace('\n', '')
+    html_table = html_table.replace("\n", "")
 
     # Print the HTML
-    output_file = PATH / '../deelnemers_table.html'
-    with open(output_file, 'w') as f:
+    output_file = PATH / "../deelnemers_table.html"
+    with open(output_file, "w") as f:
         f.writelines(html_table)
     print(f"New file written in: {output_file.resolve()}")
 
